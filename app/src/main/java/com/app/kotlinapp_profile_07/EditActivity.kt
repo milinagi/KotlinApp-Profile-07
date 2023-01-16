d92ced97d8a7a1fe2b4473d8d5d98b88115d28e6
JorgeRC <jorgercdeveloper@gmail.com>
2023-01-16 18:47:52 +0000

    Finish profile function

--- app/src/main/java/com/app/kotlinapp_profile_07/EditActivity.kt
+++ app/src/main/java/com/app/kotlinapp_profile_07/EditActivity.kt
@@ -1,14 +1,53 @@
 package com.app.kotlinapp_profile_07
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import com.app.kotlinapp_profile_07.databinding.ActivityEditBinding
 
 class EditActivity : AppCompatActivity() {
+
+    private  lateinit var binding: ActivityEditBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit)
+        binding = ActivityEditBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        with(binding) {
+            intent.extras?.let {
+
+                etName.setText(it.getString(getString(R.string.key_name)))
+                etEmail.setText(it.getString(getString(R.string.key_email)))
+                etWebsite.setText(it.getString(getString(R.string.key_website)))
+                etPhone.setText(it.getString(getString(R.string.key_phone)))
+                etLat.setText(it.getDouble(getString(R.string.key_latitude)).toString())
+                etLong.setText(it.getDouble(getString(R.string.key_longitude)).toString())
+            }
+
+            etName.setOnFocusChangeListener { v, hasFocus ->
+                if (hasFocus) { etName.text?.let { etName.setSelection(it.length) } }
+            }
+            etEmail.setOnFocusChangeListener { v, hasFocus ->
+                if (hasFocus) { etEmail.text?.let { etEmail.setSelection(it.length) } }
+            }
+            etWebsite.setOnFocusChangeListener { v, hasFocus ->
+                if (hasFocus) { etWebsite.text?.let { etWebsite.setSelection(it.length) } }
+            }
+            etPhone.setOnFocusChangeListener { v, hasFocus ->
+                if (hasFocus) { etPhone.text?.let { etPhone.setSelection(it.length) } }
+            }
+            etLat.setOnFocusChangeListener { v, hasFocus ->
+                if (hasFocus) { etLat.text?.let { etLat.setSelection(it.length) } }
+            }
+            etLong.setOnFocusChangeListener { v, hasFocus ->
+                if (hasFocus) { etLong.text?.let { etLong.setSelection(it.length) } }
+            }
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -17,9 +56,37 @@ class EditActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.action_save) {
-            finish()
+        when(item.itemId) {
+            android.R.id.home -> onBackPressed()
+            R.id.action_save -> sendData()
         }
+//        if (item.itemId == R.id.action_save) {
+//            sendData()
+//        } else if (item.itemId == android.R.id.home) {
+//            onBackPressed()
+//        }
         return super.onOptionsItemSelected(item)
+    }
+
+    fun sendData() {
+        val intent = Intent()
+
+        with(binding) {
+
+            intent.apply {
+                putExtra(getString(R.string.key_name), etName.text.toString())
+                putExtra(getString(R.string.key_email), etEmail.text.toString())
+                putExtra(getString(R.string.key_website), etWebsite.text.toString())
+                putExtra(getString(R.string.key_phone), etPhone.text.toString())
+                putExtra(getString(R.string.key_latitude), etLat.text.toString().toDouble())
+                putExtra(getString(R.string.key_longitude), etLong.text.toString().toDouble())
+            }
+        }
+
+//        intent.putExtra(getString(R.string.key_name), binding.etName.text.toString())
+
+
+        setResult(RESULT_OK, intent)
+        finish()
     }
 }
--- app/src/main/java/com/app/kotlinapp_profile_07/MainActivity.kt
+++ app/src/main/java/com/app/kotlinapp_profile_07/MainActivity.kt
@@ -34,8 +34,8 @@ class MainActivity : AppCompatActivity() {
         binding.tvEmail.text = email
         binding.tvWebsite.text = website
         binding.tvPhone.text = phone
-        lat = 28.2542
-        long = -16.6189
+//        lat = 28.2542
+//        long = -16.6189
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -45,9 +45,39 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.action_id) {
-            startActivity(Intent(this, EditActivity::class.java))
+            val intent = Intent(this, EditActivity::class.java)
+            intent.putExtra(getString(R.string.key_name), binding.tvName.text)
+            intent.putExtra(getString(R.string.key_email), binding.tvEmail.text.toString())
+            intent.putExtra(getString(R.string.key_website), binding.tvWebsite.text.toString())
+            intent.putExtra(getString(R.string.key_phone), binding.tvPhone.text)
+            intent.putExtra(getString(R.string.key_latitude), lat)
+            intent.putExtra(getString(R.string.key_longitude), long)
+
+//            startActivity(intent) <----- solo lanzmaiento
+            startActivityForResult(intent, RC_EDIT) // <----- lanzamiento y espera de respuesta
         }
         return super.onOptionsItemSelected(item)
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == RC_EDIT) {
+                val name = data?.getStringExtra(getString(R.string.key_name))
+                val email = data?.getStringExtra(getString(R.string.key_email))
+                val website = data?.getStringExtra(getString(R.string.key_website))
+                val phone = data?.getStringExtra(getString(R.string.key_phone))
+                lat = data?.getDoubleExtra(getString(R.string.key_latitude), 0.0) ?: 0.0
+                long = data?.getDoubleExtra(getString(R.string.key_longitude), 0.0) ?: 0.0
+
+                updateUI(name!!, email!!, website!!, phone!!)
+            }
+        }
+    }
+
+    companion object {
+        private const val RC_EDIT = 13
     }
 }
